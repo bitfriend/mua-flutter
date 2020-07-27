f7edc1f038ce11b93cdb0a8ffb922951c5b4761e
bitfriend <bitcapture@hotmail.com>
2020-07-28 02:45:52 +0800

    Improve status bar color

--- lib/scenes/CreateAccount.dart
+++ lib/scenes/CreateAccount.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../theme/light.dart';
 
@@ -12,9 +12,7 @@ class CreateAccount extends StatefulWidget {
 class CreateAccountState extends State<CreateAccount> {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarIconBrightness: Brightness.dark,
-    ));
+    FlutterStatusbarcolor.setStatusBarWhiteForeground(false);
     ScreenUtil.init(context, width: 375, height: 812);
     return Scaffold(
       appBar: AppBar(
